37095f4315f185671b4583d2ca06b037019dea78
Ruvin Roshan <ruvinroshan@gmail.com>
2020-11-08 15:38:20 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+ <b-container class="bv-homa-page">
+   <b-row>
+    <b-col> <LeadPassengerSingle /></b-col>
+   
+  </b-row>
+<!-- 
+  <b-row>
+    <b-col>    <LeadPassengerList /></b-col>   
+  </b-row> -->
+
+
+</b-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
